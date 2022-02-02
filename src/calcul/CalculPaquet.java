22946adb9ca3caa351506338b835c59d9fe269cd
nisab3 <37090969+nisab3@users.noreply.github.com>
2022-02-02 14:11:49 -0500

    squelette CalculPaquet
    ajout des methodes et commentaires

--- src/calcul/CalculPaquet.java
+++ src/calcul/CalculPaquet.java
@@ -33,12 +33,45 @@ public class CalculPaquet {
 	 * @param chemin  Le chemin a partir du dossier source du projet pour trouver le paquet (n'inclut pas celui du paquet)
 	 * @param nomPaquet  Nom du paquet
 	 */
-	public calculPaquet(String chemin, String nomPaquet) {
+	public CalculPaquet(String chemin, String nomPaquet) {
 		super();
 		this.chemin = chemin;
 		this.nomPaquet = nomPaquet;
 	}
 	
-	public get
+	/**
+	 * Methode pour avoir la liste des metriques du paquet et de ses classes
+	 * 
+	 * @return String[] ou le premier element est les metriques du paquet, ensuite de tout les classes.
+	 */
+	public String[] getMetrique() {
+		//TODO concatenner les metriques du paquet et des classes
+		String[] resultat = {"paquet", "classe", "classe"};
+		return resultat;
+	}
 	
+	/** 
+	 * Methode private pour recehrcher dans le paquet chaque classe et lancer le calcul sur chacune d'elle.
+	 * Les metriques des classes et celle du paquet seront sauvegarder dans l'objet
+	 */
+	private void parcourir() {
+		//TODO rechercher tout les classes et les annalyser une apres l'autre
+		//TODO enregistrer les metriques de classes
+		//TODO enresistrer les metrique de paquet
+	}
+	
+	/** 
+	 * Methode private pour ajouter les metriques d'une nouvelle classe trouver au total du paquet
+	 */
+	private void addMetriquePaquet(String[] classe) {
+		//TODO additionner les metriques de cette classe a celle total du paquet
+	}
+	
+	/** 
+	 * Methode private pour ajouter les metriques d'une nouvelle classe trouver a la liste des classes
+	 */
+	private void addMetriqueClasse(String[] classe) {
+		//TODO additionner les metriques de cette classe a la liste des classes
+	}
+
 }
