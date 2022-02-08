15c6f91c6c640f7120ba2cf65ce651cdc70d9d64
DS-A8 <78123786+DS-A8@users.noreply.github.com>
2022-02-08 16:14:20 -0500

    Methode Analyse et les sous methodes liees

--- src/calcul/AnalyseLigne.java
+++ src/calcul/AnalyseLigne.java
@@ -3,11 +3,16 @@
  * @author Dave Sanon-Abraham
  *
  */
+
 package calcul;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.ArrayList;
+
+
 /**
  * Classe pour faire l'analyse des lignes de code pour savoir s'il y a:
  * - du code ou rien
@@ -23,29 +28,39 @@ import java.util.Properties;
  *    8  ligne de code
  *    8/ fin d'un commentaire
  */
+
 public class AnalyseLigne {
 	
-	/** liste de bool de l'analyse {code ou rien, comment end_of_line, start bloc, end bloc}. */
+	/** liste de bool de l'analyse {code, commentaire , bloc}. */
 	private boolean[] resultat;
 	
 	/** caracteres qui represente les comemntaires end_of_line */
-	private String[] endOfLine;
+	private ArrayList <String> endOfLine;
 	
 	/** caracteres qui represente le debut d'un bloc */
-	private String[] start;
+	private ArrayList <String> start;
 	
 	/** caracteres qui represente la fin d'un bloc */
-	private String[] end;
+	private ArrayList <String> end;
+	
+	/** boolean indicant la presence d'un bloc */
+	private boolean bloc;
+	
+	/** int indicant le nombre de commentaires imbriques*/
+	private int imbrique;
 
 	/**
 	 * Constructeur de analyseLigne
 	 * nous allons chercher dans le fichier config les caracteres qui represente les commentaires
 	 */
 	public AnalyseLigne() {
-		this.resultat = new boolean[4];
+		this.resultat = new boolean[3]; // {code, commentaire, bloc}
+		this.bloc = false;
+		this.imbrique = 0;
 		this.reset();
 		
-		// loader les properties
+
+		/*// loader les properties
 		Properties config = new Properties();
 		try { 
 			FileInputStream fis = new FileInputStream("src/calcul/config.properties");
@@ -61,13 +76,25 @@ public class AnalyseLigne {
 		} catch (IOException io) {
 			io.printStackTrace();
 		}
+*/
+		//TODO aller chercher dans le fichier config les caractere qui qui represente un commentaire
+		// et les mettre dans les variables de la classe
+		
+		// hardcode for the moment
+		this.endOfLine.add("//");
+		this.start.add("/*");
+		this.start.add("/**");
+		this.end.add("*/");
+
 	}
 	
 	/** 
 	 * Methode pour remettre la liste de resultat a false.
 	 */
 	private void reset() {
-		Arrays.fill(resultat, Boolean.FALSE);
+		resultat[0]= false;
+		resultat[1]= false;
+		resultat[2]= bloc;
 	}
 	
 	
@@ -78,17 +105,86 @@ public class AnalyseLigne {
 	 * et aussi des bebut et fin de bloc de commentaire
 	 * 
 	 * @param ligne  String de la ligne de code a analyser
-	 * @return  LA liste des resultats trouver dans la ligne {code ou rien, comment end_of_line, start bloc, end bloc}
+	 * @return  LA liste des resultats trouver dans la ligne 
+	 *          {code ou rien, comment ou rien, start bloc, end bloc}
 	 */
 	public boolean[] analyse(String ligne) {
-		this.reset(); 					//remettre resultat a false
-		//TODO lire la ligne pour retourner ce qui est trouver
-		return resultat;
+		
+		this.reset();   		//remettre resultat a false
+		ligne.trim();			//enlever les espaces de debut de ligne
+		
+		if (vide(ligne)) return resultat;  	// ligne vide
+		resultat[0]= code(ligne);			// ligne avec code	
+		resultat[1]= commentaire(ligne);	// ligne avec commentaire
+		resultat[2]= bloc;					// presence d'un bloc
+		return resultat; 
+	
 	}
 	
+
 	public static void main(String[] args) {
 		AnalyseLigne test = new AnalyseLigne();
 		
 	}
+
+	/* Methode qui prend en parametre une ligne du fichier et retourne le bool indiquant
+	 * si la ligne contient du code
+	 */
+	private boolean code(String ligne) {
+		if (bloc==false && commentaireDebut(ligne)==false ) return true;
+		return false;
+	}
+	
+	/* Methode qui prend en parametre une ligne du fichier et retourne le bool indiquant
+	 * si la ligne contient un commentaire
+	 */
+	private boolean commentaire (String ligne) {
+		boolean blocComment = commentaire(ligne,start); // presence d'un bloc
+		boolean endComment = commentaire(ligne,endOfLine); //presence endOfLine
+		boolean endBloc = commentaire(ligne,end); // fin d'un bloc
+		imbrication(blocComment,endBloc);
+		
+		if (endComment|| blocComment || endBloc || bloc) return true;
+		else return false;
+	}
+	
+	/* Methode qui met a jour le nombre d'imbrication de commentaire et la 
+	 * presence d'un bloc. Prend en parametre les bool represent la presence
+	 * d'une ouverture de bloc et d'une fermeture de bloc respectivement
+	 */
+	private void imbrication(boolean blocComment, boolean endBloc) {
+		if(blocComment) imbrique++;
+		if(endBloc) imbrique--;
+		if(imbrique==0) bloc= false; 
+		else bloc=true;
+		
+	}
+	
+	/* Methode determinant si le String du premier parametre contient un
+	 * element de la liste passee en second parametre
+	 */
+	private boolean commentaire(String ligne, ArrayList <String> symbols) {
+		for(int i=0; i<symbols.size(); i++) {
+			if (ligne.contains(symbols.get(i))) return true;
+		}
+		return false;
+	}
+	
+	/* Methode determinant si le String en parametre est vide */
+	private boolean vide (String ligne) {
+		if(ligne=="") return true;
+		else return false;
+	}
+	
+	/* Methode retournant le bool reprensant si le String en parametre contient 
+	 * un symbole de commentaire en debut de ligne
+	 */
+	private boolean commentaireDebut(String ligne) {
+		for (int i = 0; i< start.size(); i++) {
+			if(ligne.indexOf(start.get(i))== 0) return true;
+		}
+		return false;
+	}
+
 	
 }
